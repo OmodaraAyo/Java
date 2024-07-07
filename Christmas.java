16442ccf506c473053c461174bb5788b41022aaa
OmodaraAyo <ayodeleomodara1234@gmail.com>
2024-07-07 08:46:59 -0700

    Add files via upload

---
+++ Christmas.java
@@ -0,0 +1,114 @@
+public class Christmas {
+
+	public static void main ( String [] args ) {
+
+	int days = 13;
+
+		for ( int i = 1 ; i < days; i++ )  { 
+
+			switch (  i ) {
+
+				case 1: 
+					System.out.println ( " On the first day of Christmas, " );
+					System.out.println ( " my true love gave to me " );
+				break;
+				case 2:
+					System.out.println ( "\n On the second day of Christmas," );
+					System.out.println ( " my true love gave to me " );
+				break;
+				case 3:
+					System.out.println ( "\n On the third day of Christmas, " );
+					System.out.println ( " my true love gave to me " );
+				break;
+				case 4: 
+					System.out.println ( "\n On the fourth day of Christmas, " );
+					System.out.println ( " my true love gave to me " );
+				break;
+				case 5: 
+					System.out.println ( "\n On the fifth day of Christmas, " );
+					System.out.println ( " my true love gave to me " );
+				break;
+				case 6: 
+					System.out.println ( "\n On the sixth day of Christmas, " );
+					System.out.println ( " my true love gave to me " );
+				break;
+				case 7: 
+					System.out.println ( "\n On the seventh day of Christmas, " );
+					System.out.println ( " my true love gave to me " );
+				break;
+				case 8: 
+					System.out.println ( "\n On the eighth day of Christmas, " );
+					System.out.println ( " my true love gave to me " );
+				break;
+				case 9:
+					System.out.println ( "\n On the ninth day of Christmas, " );
+					System.out.println ( " my true love gave to me " );
+				break;
+				case 10:
+					System.out.println ( "\n On the tenth day of Christmas, " );
+					System.out.println ( " my true love gave to me " );
+				break;
+				case 11:
+					System.out.println ( "\n On the eleventh day of Christmas, " );
+					System.out.println ( " my true love gave to me " );
+				break;
+				case 12:
+					System.out.println ( "\n On the twelfth day of Christmas," );
+					System.out.println ( " my true love gave to me " );
+				break;
+
+			}
+		
+		
+			switch ( i ) {
+				case 12:
+					System.out.println ( " Twelve drummers drumming, " );
+					
+				case 11: 
+					System.out.println ( " Eleven pipers piping, " );
+
+				case 10:
+					System.out.println ( " Ten lords a-leaping, " );
+
+				case 9: 
+					System.out.println ( " Nine ladies dancing, "  );
+
+				case 8: 
+					System.out.println ( " Eight maids a-milking, " );
+					
+				case 7:
+					System.out.println ( " Seven swans a-swimming, " );
+					
+				case 6: 
+					System.out.println ( " Six geese a-laying, " );
+				
+				case 5: 
+					System.out.println ( " Five golden rings, " );
+
+				case 4:
+					System.out.println ( " Four calling birds, " );
+					
+				case 3:
+					System.out.println ( " Three French hens, " );
+				
+				case 2:
+					System.out.println ( " Two turtle doves, " );
+
+				case 1: 
+					System.out.println ( " A partridge in a pear tree. " );
+					
+
+			}
+
+ 
+
+		       }
+		}
+	}
+				
+
+
+
+
+
+
