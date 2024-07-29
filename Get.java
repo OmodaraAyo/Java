7f691cbf79adda81e2da9b2ab3466dd4c9500979
Omodara Emmanuel Ayodele <ayodeleomodara1234@gmail.com>
2024-07-29 05:18:24 +0100

    Add files via upload

---
+++ Get.java
@@ -0,0 +1,14 @@
+public class Get{
+
+	public static int largestElementIn(int [] number){
+
+		int largest = 0;		
+		for(int value = 0; value < number.length; value++){
+			largest = number;
+			if (largest > number){
+				return largest;
+			}
+
+		}
+	}
+}
---
+++ LargestNumber.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class LargestNumber{
+
+	public static void main (String... args){
+	
+	int [] number = {1,54,2,5,67,21};
+
+	int result = Get.largestElementIn(number);
+	System.out.print(Arrays.toString(result));
+   }
+}
